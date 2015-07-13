e232d875ea581084dc44caa7c8eea6e2cbb2dfaa
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-07-13 15:47:15 -0700

    Adds sessions controller create action

--- run_tests.sh
+++ run_tests.sh
@@ -3,10 +3,13 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
   elif [ $1 = "users_controller" ]
     then
-    bundle exec rspec spec/controllers/api/v1/users_controller_spec.rb
+      bundle exec rspec spec/controllers/api/v1/users_controller_spec.rb
   elif [ $1 = "users_model" ]
     then
-    bundle exec rspec spec/models/user_spec.rb
+      bundle exec rspec spec/models/user_spec.rb
+  elif [ $1 = "sessions_controller" ]
+    then
+      bundle exec rspec spec/controllers/api/v1/sessions_controller_spec.rb
   else
     echo "available tests are: users_controller, users_model"
 fi
