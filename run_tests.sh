c7e9a0036392a9f65b7021b9277349daaaec4efb
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-07-13 15:31:48 -0700

    Adds user authentication token

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,12 @@
-bundle exec rspec spec/controllers/api/v1/users_controller_spec.rb
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+  elif [ $1 = "users_controller" ]
+    then
+    bundle exec rspec spec/controllers/api/v1/users_controller_spec.rb
+  elif [ $1 = "users_model" ]
+    then
+    bundle exec rspec spec/models/user_spec.rb
+  else
+    echo "available tests are: users_controller, users_model"
+fi
