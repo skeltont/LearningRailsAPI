9ec5c4f2500a7a5af04b9cbd14b7db33c21069b5
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-07-14 14:41:18 -0700

    Adds authenticable module for managing authentication methods

--- run_tests.sh
+++ run_tests.sh
@@ -10,6 +10,9 @@ if [ $# -eq 0 ]
   elif [ $1 = "sessions_controller" ]
     then
       bundle exec rspec spec/controllers/api/v1/sessions_controller_spec.rb
+  elif [ $1 = "authenticable_spec" ]
+    then
+      bundle exec rspec spec/controllers/concerns/authenticable_spec.rb
   else
     echo "available tests are: users_controller, users_model"
 fi
