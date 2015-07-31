b047ff798d0497ae29c243ac9a4f54ecddbbc5d5
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-07-31 11:20:33 -0700

    adds the destroy action

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create]
+  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
   respond_to :json
 
   def index
@@ -26,6 +26,12 @@ class Api::V1::ProductsController < ApplicationController
     else
       render json: { errors: product.errors }, status: 422
     end
+  end
+
+  def destroy
+    product = current_user.products.find(params[:id])
+    product.destroy
+    head 204
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
                 constraints: ApiConstraints.new(version: 1, default: true) do
       # we are going to list our resources here
       resources :users, :only => [:show, :create, :update, :destroy] do
-        resources :products, :only => [:create, :update]
+        resources :products, :only => [:create, :update, :destroy]
       end
       resources :sessions, :only => [:create, :destroy]
       resources :products, :only => [:show, :index]
